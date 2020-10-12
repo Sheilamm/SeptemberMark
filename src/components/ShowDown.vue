dcae188949ab308a0ae2653556ea82910d75bf17
sheila <maowei@sobey.com>
2020-10-12 17:16:13 +0800

    解决导出pdf空表文档报错的问题

--- src/components/ShowDown.vue
+++ src/components/ShowDown.vue
@@ -269,7 +269,7 @@ export default {
     },
 
     makeMpdf(fileName) {
-      const element = document.querySelector('.md-body');
+      const element = document.querySelector('.markdown-body');
       html2canvas(element, {
         useCORS: true,
         logging: false,
@@ -277,19 +277,20 @@ export default {
         taintTest: true,
         timeout: 500,
       }).then((canvas) => {
-        const pdf = new jsPDF('p', 'mm', 'a4'); //A4纸，纵向
+        const pdf = new jsPDF('p', 'mm', 'a4');
         const ctx = canvas.getContext('2d'),
           a4w = 190,
           a4h = 277, //A4大小，210mm x 297mm，四边各保留10mm的边距，显示区域190x277
           imgHeight = Math.floor((a4h * canvas.width) / a4w); //按A4显示比例换算一页图像的像素高度
         let renderedHeight = 0;
 
+        
+
         while (renderedHeight < canvas.height) {
           const page = document.createElement('canvas');
           page.width = canvas.width;
           page.height = Math.min(imgHeight, canvas.height - renderedHeight); //可能内容不足一页
 
-          //用getImageData剪裁指定区域，并画到前面创建的canvas对象中
           page
             .getContext('2d')
             .putImageData(
